2bfbe9fbe69de8076dec39d9b7c99b42628b2704
youxuan14310 <xuan.you@hand-china.com>
2017-10-18 14:49:47 +0800

    14:49

--- src/containers/finance-setting/beep-timer/beep-from.js
+++ src/containers/finance-setting/beep-timer/beep-from.js
@@ -3,8 +3,17 @@
  */
 import React from 'react'
 import { connect } from 'react-redux'
-import { Form, Input, Switch, Button, Icon ,Tabs,Row,Col} from 'antd'
-import Row from "antd/lib/grid/row.d";
+import { injectIntl } from 'react-intl';
+
+import { Form, Input, Switch, Button, Icon ,Tabs,Row,Col,message,Checkbox} from 'antd'
+const CheckboxGroup = Checkbox.Group;
+import httpFetch from 'share/httpFetch'
+import config from 'config'
+import menuRoute from 'share/menuRoute'
+
+import 'styles/finance-setting/beep-timer/beep-from.scss'
+
+
 const FormItem = Form.Item;
 const TabPane =Tabs.TabPane;
 
@@ -13,6 +22,12 @@ class BeepFrom extends React.Component{
     super(props);
     this.state = {
 
+     options : [
+        { label: '差旅申请单', value: 'ee' },
+        { label: '国际出差申请单', value: 'pp' },
+        { label: '差旅申请单', value: 'Orange' },
+       { label: '国际出差申请单', value: 'Orange2' },
+      ]
     };
   }
 
@@ -34,51 +49,103 @@ class BeepFrom extends React.Component{
     const { getFieldDecorator } = this.props.form;
     const {} = this.state;
     const formItemLayout = {
-      labelCol: { span: 6 },
-      wrapperCol: { span: 14, offset: 1 },
+      labelCol: { span:24 },
+      wrapperCol: { span:24 },
     };
+
+    const formItemLayout2 ={
+      labelCol: { span:16 },
+      wrapperCol: { span:16 },
+    }
     return (
-      <div className="beep-timer">
-        <Form >
-          <Row>
-            <FormItem {...formItemLayout} label={this.props.intl.formatMessage({id:"budget.journalCode"})} >
-              {getFieldDecorator('journalCode', {
-                rules: [{
-                }],
-                initialValue: '-'
-              })(
-                <Input  disabled={true} />
-              )}
-            </FormItem>
-          </Row>
-          <Row>
-              <FormItem {...formItemLayout} label={this.props.intl.formatMessage({id:"budget.journalCode"})} >
-                {getFieldDecorator('journalCode', {
+      <div className="beep-from">
+        <div className="beep-from-in">
+          <Form >
+            <Row>
+              <Col span={8}>
+              <FormItem {...formItemLayout} label="提醒标题" >
+                {getFieldDecorator('journalCodeffr', {
                   rules: [{
                   }],
                   initialValue: '-'
                 })(
-                  <Input  disabled={true} />
+                  <Input  />
                 )}
               </FormItem>
-          </Row>
-          <Row>
-            <Col></Col>
+              </Col>
+            </Row>
+            <Row>
 
-            <Col></Col>
+              <Col span={8}>
+              <FormItem {...formItemLayout} label="提醒内容" >
+                {getFieldDecorator('journalCodwefwee', {
+                  rules: [{
+                  }],
+                  initialValue: ''
+                })(
+                  <Input type="text"  />
+                )}
+              </FormItem>
+              </Col>
+            </Row>
+            <Row>
+              <Col span={8}>
 
-          </Row>
-          <Row>
+                <FormItem {...formItemLayout} label="发送日期" >
+                  <span>预计还款日期前</span>
+                  {getFieldDecorator('journalCode2', {
+                    rules: [{
+                    }],
+                    initialValue: ''
+                  })(
+                    <Input style={{width:40}} />
+                  )}
+                  <span> 天提醒员工</span>
+                </FormItem>
 
-          </Row>
-          <Row>
-            <FormItem wrapperCol={{ offset: 7 }}>
-              <Button type="primary" htmlType="submit" loading={this.state.loading} style={{marginRight:'10px'}}>保存</Button>
-              <Button>取消</Button>
-            </FormItem>
-          </Row>
+              </Col>
 
-        </Form>
+              <Col span={8}>
+                <FormItem {...formItemLayout} label="发送时间" >
+                  {getFieldDecorator('jou', {
+                    rules: [{
+                    }],
+                    initialValue: ''
+                  })(
+                    <Input />
+                  )}
+                </FormItem>
+
+              </Col>
+
+            </Row>
+            <Row>
+              <Col>
+
+                <FormItem {...formItemLayout} label="适用单据" >
+                  {getFieldDecorator('cdcsd', {
+                    rules: [{
+                    }],
+                    initialValue: ''
+                  })(
+                    <CheckboxGroup options={this.state.options}  />
+
+                  )}
+
+                </FormItem>
+
+              </Col>
+
+            </Row>
+            <Row>
+              <FormItem wrapperCol={{ offset: 0 }}>
+                <Button type="primary" htmlType="submit" loading={this.state.loading} style={{marginRight:'10px'}}>保存</Button>
+                <Button>取消</Button>
+              </FormItem>
+            </Row>
+
+          </Form>
+        </div>
       </div>
     )
   }
@@ -91,3 +158,7 @@ function mapStateToProps(state) {
 const WrappedBeepFrom= Form.create()(BeepFrom);
 
 export default connect(mapStateToProps)(WrappedBeepFrom);
+
+
+
+
