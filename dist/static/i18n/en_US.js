7fbb239a4f9c78f339db1aa0007ad486c89a628d
Vance <vancezhao@gmail.com>
2017-10-18 11:31:22 +0800

    1. FIx docker config 2. Remove dist folder

---
+++ dist/static/i18n/en_US.js
@@ -0,0 +1,48 @@
+const en_US = {
+  "budget.structure":"Budget structure",
+  "budget.structureCode": "Budget structure code",
+  "budget.structureName": "Budget structure name",
+  "periodStrategy":"Period strategy",
+  "budget.structureDescription": "Budget structure description"
+};
+
+import common from 'static/i18n/en_US/common.json'
+import menu from 'static/i18n/en_US/menu.json'
+import budgetOrganization from 'static/i18n/en_US/budget-setting/budget-organization/budget-organization.json'
+import login from 'static/i18n/en_US/login.json'
+import budgetStructure from 'static/i18n/en_US/budget-setting/budget-organization/budget-organization.json'
+import budgetItem from 'static/i18n/en_US/budget-setting/budget-organization/budget-item.json'
+import budgetControlRules from 'static/i18n/en_US/budget-setting/budget-organization/budget-control-rules.json'
+import payWorkbench from 'static/i18n/en_US/pay/pay-workbench.json'
+import budgetVersion from 'static/i18n/en_US/budget-setting/budget-organization/budget-version.json'
+import budgetItemType from  'static/i18n/en_US/budget-setting/budget-organization/budget-item-type.json'
+import budgetJournal from 'static/i18n/en_US/budget-journal/budget-journal.json'
+import bankDefinition from 'static/i18n/en_US/pay/bank-definition.json'
+import agencySetting from 'static/i18n/en_US/approve-setting/agency-setting.json'
+import securitySetting from 'static/i18n/en_US/setting/security-setting.json'
+
+const i18nList = [
+  common,  //公用
+  login,  //登录及主界面
+  menu,  //菜单
+  budgetOrganization,  //预算组织
+  budgetStructure,  //预算表
+  budgetItem,   //预算项目
+  budgetControlRules, //预算控制规则
+  payWorkbench, //付款工作台
+  budgetVersion, //预算版本
+  budgetItemType, //预算项目类型
+  budgetJournal,   //预算日记账
+  bankDefinition, //银行定义
+  agencySetting, //代理设置
+  securitySetting //安全配置
+
+];
+
+let result = {};
+
+i18nList.map(i18n => {
+  result = Object.assign(result, i18n)
+});
+
+export default result
