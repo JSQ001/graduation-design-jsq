8190698e54ab7b2a872e707402a2f41929b4b67b
ZaraNengap <ZaraNengap@163.com>
2017-11-02 17:11:46 +0800

    fixed ac

--- setenv.sh
+++ setenv.sh
@@ -27,6 +27,16 @@ server {
         proxy_set_header Host \$host;
         client_max_body_size 10m;
     }
+    location /api/budget/ {
+            proxy_pass $BUDGET_URL/api/;
+            proxy_set_header Host \$host;
+            client_max_body_size 10m;
+    }
+    location /api/cash/ {
+                proxy_pass PAY_URL/api/;
+                proxy_set_header Host \$host;
+                client_max_body_size 10m;
+    }
 
 #    location /brms/ {
 #        proxy_pass $BRMS_URL/brms;
