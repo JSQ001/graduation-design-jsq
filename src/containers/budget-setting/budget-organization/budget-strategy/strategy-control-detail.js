0fd14a11b56204987b0fc3ab1f76cc6f712ed0c7
蒋松秋 <songqiu.jiang@hand-china.com>
2017-12-13 16:31:35 +0800

    合并develop前

--- src/containers/budget-setting/budget-organization/budget-strategy/strategy-control-detail.js
+++ src/containers/budget-setting/budget-organization/budget-strategy/strategy-control-detail.js
@@ -16,6 +16,7 @@ import 'styles/budget-setting/budget-organization/budget-strategy/strategy-contr
 class StrategyControlDetail extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
       loading: false,
       strategyControlId: null,
