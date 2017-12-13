8aa37b6359b3d0a5087b8fbda6460bda1607604f
蒋松秋 <songqiu.jiang@hand-china.com>
2017-12-13 16:28:03 +0800

    12131627

---
+++ src/components/importer.js
@@ -0,0 +1,233 @@
+import React from 'react';
+import { connect } from 'react-redux'
+import { injectIntl } from 'react-intl'
+import config from 'config'
+import { Modal, Button, Tabs, Upload, Icon, message, Table } from 'antd'
+const TabPane = Tabs.TabPane;
+import httpFetch from 'share/httpFetch'
+import FileSaver from 'file-saver'
+
+//数据导入组件
+class Importer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: false,
+      fileList: [],
+      uploading: false,
+      tabKey: 'UPDATE',
+      result: {},
+      transactionID: null,
+      errorColumns: [
+        {title: '行号', dataIndex: 'index', width: '10%'},
+        {title: '错误信息', dataIndex: 'error'}
+      ],
+      errorData: []
+    };
+  }
+
+  handleOk = () => {
+    const { tabKey } = this.state;
+    //在导入tab下为上传
+    if(tabKey === 'UPDATE'){
+      const { fileList } = this.state;
+      const formData = new FormData();
+      fileList.forEach((file) => {
+        formData.append('file', file);
+      });
+      this.setState({
+        uploading: true,
+      });
+      //TODO:导入数据，根据结果跳转success tab，显示成功与失败的结果
+      httpFetch.post(this.props.uploadUrl, formData, {"Content-type": 'multipart/form-data'}).then(res => {
+        this.setState({ transactionID: res.data.transactionID },() => {
+          this.listenStatus()
+        });
+      }).catch(() => {
+        this.setState({ uploading: false });
+        message.error('导入失败，请重试')
+      })
+    } else {
+      this.props.onOk('close');
+      this.setState({
+        visible: false,
+        tabKey: 'UPDATE'
+      })
+    }
+  };
+
+  //监听导入状态，status === 1003时表示成功
+  listenStatus = () => {
+    httpFetch.get(`${config.budgetUrl}/api/batch/transaction/logs/${this.state.transactionID}`).then(res => {
+      if (res.data.status !== 1003) {
+        this.listenStatus()
+      } else {
+        this.setState({
+          fileList: [],
+          tabKey: 'SUCCESS',
+          uploading: false,
+          result: res.data
+        }, () => {
+          let errorData = [];
+          let errors = this.state.result.errors;
+          Object.keys(errors).map(error => {
+            errors[error].map(index => {
+              if (errorData.length) {
+                errorData.map((item, i) => {
+                  if (index === item.index) {
+                    errorData[i].erros = error
+                  } else {
+                    errorData.push({index, error})
+                  }
+                })
+              } else {
+                errorData.push({index, error})
+              }
+            })
+          });
+          this.setState({ errorData })
+        })
+      }
+    }).catch(() => {
+      this.setState({ uploading: false });
+      message.error('导入失败，请重试')
+    })
+  };
+
+  showImporter = () => {
+    this.setState({visible: true, tabKey: 'UPDATE'})
+  };
+
+  onCancel = () => {
+    this.setState({visible: false});
+    if (this.state.uploading) {
+      httpFetch.delete(`${config.budgetUrl}/api/batch/transaction/logs/${this.state.transactionID}`)
+    }
+  };
+
+  //TODO:下载表格
+  downloadTemplate = () => {
+    let hide = message.loading('正在生成文件..');
+    httpFetch.get(this.props.templateUrl, {}, {}, {responseType: 'arraybuffer'}).then(res => {
+      let b = new Blob([res.data], {type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"});
+      let name = this.props.fileName;
+      FileSaver.saveAs(b, `${name}.xlsx`);
+      hide();
+    }).catch(() => {
+      message.error('表格下载失败，请重试');
+      hide();
+    })
+  };
+
+  //下载错误信息
+  downloadErrors = () => {
+    let hide = message.loading('正在生成文件..');
+    let url = this.props.errorUrl + `/${this.state.transactionID}`;
+    httpFetch.get(url, {}, {}, {responseType: 'arraybuffer'}).then(res => {
+      let b = new Blob([res.data], {type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"});
+      let name = '错误信息';
+      FileSaver.saveAs(b, `${name}.xlsx`);
+      hide();
+    }).catch(() => {
+      message.error('错误信息下载失败，请重试');
+      hide();
+    })
+  };
+
+  render() {
+    const { title, uploadUrl } = this.props;
+    const { visible, uploading, tabKey, result, errorColumns, errorData } = this.state;
+    const props = {
+      action: uploadUrl,
+      onRemove: (file) => {
+        this.setState(({ fileList }) => {
+          const index = fileList.indexOf(file);
+          const newFileList = fileList.slice();
+          newFileList.splice(index, 1);
+          return {
+            fileList: newFileList,
+          };
+        });
+      },
+      beforeUpload: (file) => {
+        this.setState(({ fileList }) => ({
+          fileList: [...fileList, file],
+        }));
+        return false;
+      },
+      fileList: this.state.fileList,
+    };
+
+    return (
+      <Button onClick={this.showImporter}>
+        导 入
+        <Modal className="importer"
+               visible={visible}
+               onCancel={this.onCancel}
+               onOk={this.handleOk}
+               title={title}
+               confirmLoading={uploading}
+               okText={tabKey === 'UPDATE' ? '导入' : '完成'}>
+          <Tabs defaultActiveKey="UPDATE" activeKey={tabKey} onChange={(key) => {this.setState({ tabKey: key })}}>
+            <TabPane tab="上传文件" key="UPDATE" disabled={tabKey === 'SUCCESS'}>
+              <h3>创建电子表格</h3>
+              <Button size="small" type="primary" onClick={this.downloadTemplate}>下载Excel电子表格</Button><br/>
+              1.点击上方按钮<br/>
+              2.严格按照导入模板整理数据，检查必输事项是否缺少数据<br/>
+              3.关闭Excel文件后，方可进行数据导入<br/>
+              <br/>
+              <h3>上传电子表格</h3>
+              1.点击【选择文件】按钮<br/>
+              2.选择你刚更新过的Excel文件，并点击确定<br/>
+              3.点击【导入】按钮<br/>
+              <br/>
+              <Upload {...props}>
+                <Button>
+                  <Icon type="upload" /> 选择文件
+                </Button>
+              </Upload>
+            </TabPane>
+            <TabPane tab="导入结果" key="SUCCESS" disabled={tabKey === 'UPDATE'}>
+              <div>导入成功：{result.successEntities}条</div>
+              <div>导入失败：{result.failureEntities}条
+                {result.failureEntities ? <span style={{fontSize:12,marginLeft:10}}>（请修改相应数据后，重新导入）</span> : ''}
+              </div>
+              {result.failureEntities ? (
+                <div>
+                  <a style={{display:'block', textAlign:'right', marginBottom:6}}
+                     onClick={this.downloadErrors}>下载错误信息</a>
+                  <Table rowKey={record => record.index}
+                         columns={errorColumns}
+                         dataSource={errorData}
+                         bordered
+                         size="small"/>
+                </div>
+              ) : ''}
+            </TabPane>
+          </Tabs>
+        </Modal>
+      </Button>
+    );
+  }
+}
+
+Importer.propTypes = {
+  templateUrl: React.PropTypes.string,  //模版下载接口
+  uploadUrl: React.PropTypes.string,  //上传接口
+  errorUrl: React.PropTypes.string,  //错误信息下载接口
+  title: React.PropTypes.string,  //标题
+  fileName: React.PropTypes.string, //下载文件名
+  onOk: React.PropTypes.func
+};
+
+Importer.defaultProps = {
+  title: '导入',
+  fileName: '导入文件',
+  onOk: () => {}
+};
+
+function mapStateToProps() {
+  return {}
+}
+
+export default connect(mapStateToProps)(injectIntl(Importer));
---
+++ src/containers/approve/contract.js
@@ -0,0 +1,206 @@
+import React from 'react'
+import { injectIntl } from 'react-intl'
+import { Form, Tabs, Table, message, Badge } from 'antd'
+const TabPane = Tabs.TabPane;
+import config from 'config'
+import httpFetch from 'share/httpFetch'
+import menuRoute from 'share/menuRoute'
+
+import SearchArea from 'components/search-area'
+import moment from 'moment'
+
+class Contract extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading1: false,
+      loading2: false,
+      contractStatus: {
+        CANCEL: {label: '已取消', state: 'default'},
+        FINISH: {label: '已完成', state: 'success'},
+        GENERATE: {label: '编辑中', state: 'processing'},
+        HOLD: {label: '暂挂', state: 'warning'},
+        SUBMITTED: {label: '审批中', state: 'processing'},
+        REJECTED: {label: '已驳回', state: 'error'},
+        CONFIRM: {label: '已通过', state: 'success'},
+        FINISH2: {label: '已撤回', state: 'warning'}, //字段未确认
+      },
+      SearchForm: [
+        {type: 'input', id: 'contractNumber', label: '合同编号'},
+        {type: 'input', id: 'companyId', label: '申请人姓名/工号'},
+        {type: 'input', id: 'contractTypeName', label: '合同类型'},
+        {type: 'items', id: 'createdDate', items: [
+          {type: 'date', id: 'createdDateBegin', label: '提交时间从'},
+          {type: 'date', id: 'createdDateEnd', label: '提交时间至'}
+        ]},
+      ],
+      columns: [
+        {title: '序号', dataIndex: 'index', width: '7%', render:(value, record, index) => index + 1},
+        {title: '申请人', dataIndex: 'companyId'},
+        {title: '提交时间', dataIndex: 'createdDate', render: value => moment(value).format('YYYY-MM-DD')},
+        {title: '合同类型', dataIndex: 'contractTypeName'},
+        {title: '合同编号', dataIndex: 'contractNumber', width: '18%'},
+        {title: '币种', dataIndex: 'currency'},
+        {title: '金额', dataIndex: 'amount', render: this.filterMoney},
+        {title: '状态', dataIndex: 'status', width: '7%', render: value => <Badge status="success" text={this.state.contractStatus[value].label}/>},
+      ],
+      unapprovedData: [],
+      approvedData: [],
+      unapprovedPagination: {
+        total: 0
+      },
+      approvedPagination: {
+        total: 0
+      },
+      unapprovedPage: 0,
+      unapprovedPageSize: 10,
+      approvedPage: 0,
+      approvedPageSize: 10,
+      ContractDetail: menuRoute.getRouteItem('approve-contract-detail', 'key'), //合同详情
+    }
+  }
+
+  componentWillMount() {
+    return new Promise((resolve, reject) => {
+      this.getUnapprovedList(resolve, reject);
+      this.getApprovedList(resolve, reject)
+    }).catch(() => {
+      message.error('数据加载失败，请重试')
+    });
+  }
+
+  //获取未审批列表
+  getUnapprovedList = (resolve, reject) => {
+    const { unapprovedPage, unapprovedPageSize } = this.state;
+    let unapprovedUrl = `${config.contractUrl}/contract/api/contract/header/confirm/query?page=${unapprovedPage}&size=${unapprovedPageSize}`;
+    this.setState({ loading1: true });
+    httpFetch.get(unapprovedUrl).then((res) => {
+      if (res.status === 200) {
+        this.setState({
+          unapprovedData: res.data,
+          loading1: false,
+          unapprovedPagination: {
+            total: Number(res.headers['x-total-count']) ? Number(res.headers['x-total-count']) : 0,
+            current: unapprovedPage + 1,
+            onChange: this.onUnapprovedChangePaper
+          }
+        });
+        resolve()
+      }
+    }).catch(() => {
+      this.setState({ loading1: false });
+      reject()
+    })
+  };
+
+  //获取审批列表
+  getApprovedList = (resolve, reject) => {
+    const { approvedPage, approvedPageSize } = this.state;
+    let approvedUrl = `${config.contractUrl}/contract/api/contract/header/confirmEd/query?page=${approvedPage}&size=${approvedPageSize}`;
+    this.setState({ loading2: true });
+    httpFetch.get(approvedUrl).then((res) => {
+      if (res.status === 200) {
+        this.setState({
+          approvedData: res.data,
+          loading2: false,
+          approvedPagination: {
+            total: Number(res.headers['x-total-count']) ? Number(res.headers['x-total-count']) : 0,
+            current: approvedPage + 1,
+            onChange: this.onApprovedChangePaper
+          }
+        });
+        resolve()
+      }
+    }).catch(() => {
+      this.setState({ loading2: false });
+      reject()
+    })
+  };
+
+  //未审批点击页码
+  onUnapprovedChangePaper = (page) => {
+    if (page - 1 !== this.state.page) {
+      this.setState({ unapprovedPage: page - 1 }, () => {
+        this.getUnapprovedList()
+      })
+    }
+  };
+
+  //审批点击页码
+  onApprovedChangePaper = (page) => {
+    if (page - 1 !== this.state.page) {
+      this.setState({ approvedPage: page - 1 }, () => {
+        this.getApprovedList()
+      })
+    }
+  };
+
+  //未审批搜索
+  unapprovedSearch = (values) => {
+    console.log(values)
+  };
+
+  //审批搜索
+  approvedSearch = (values) => {
+    console.log(values)
+  };
+
+  //进入合同详情页
+  handleRowClick = (record) => {
+    this.context.router.push(this.state.ContractDetail.url.replace(':id', record.id))
+  };
+
+  render() {
+    const { loading1, loading2, SearchForm, columns, unapprovedData, approvedData, unapprovedPagination, approvedPagination } = this.state;
+    return (
+      <div className="approve-contract">
+        <Tabs onChange={this.handleTabsChange}>
+          <TabPane tab="未审批" key="unapproved">
+            <SearchArea searchForm={SearchForm}
+                        submitHandle={this.unapprovedSearch}/>
+            <div className="table-header">
+              <div className="table-header-title">{`共搜索到 ${unapprovedPagination.total} 条数据`}</div>
+            </div>
+            <Table rowKey={record => record.id}
+                   columns={columns}
+                   dataSource={unapprovedData}
+                   padination={unapprovedPagination}
+                   loading={loading1}
+                   onRow={record => ({
+                     onClick: () => this.handleRowClick(record)
+                   })}
+                   bordered
+                   size="middle"/>
+          </TabPane>
+          <TabPane tab="已审批" key="approved">
+            <SearchArea searchForm={SearchForm}
+                        submitHandle={this.approvedSearch}/>
+            <div className="table-header">
+              <div className="table-header-title">{`共搜索到 ${approvedPagination.total} 条数据`}</div>
+            </div>
+            <Table rowKey={record => record.id}
+                   columns={columns}
+                   dataSource={approvedData}
+                   padination={approvedPagination}
+                   loading={loading2}
+                   onRow={record => ({
+                     onClick: () => this.handleRowClick(record)
+                   })}
+                   bordered
+                   size="middle"/>
+          </TabPane>
+        </Tabs>
+      </div>
+    )
+  }
+}
+
+Contract.contextTypes = {
+  router: React.PropTypes.object
+};
+
+const wrappedContract = Form.create()(injectIntl(Contract));
+
+export default wrappedContract
+
+
