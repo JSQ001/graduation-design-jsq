d44b5592118e8bd8a89bcc1c9c1406d6d74e0135
youxuan14310 <xuan.you@hand-china.com>
2017-12-15 21:10:40 +0800

    修改人员bug

--- src/containers/budget/budget-journal/new-budget-journal-detail.js
+++ src/containers/budget/budget-journal/new-budget-journal-detail.js
@@ -569,7 +569,8 @@ class NewBudgetJournalDetail extends React.Component {
           }
         }
         if (item.type === 'list') {
-          if (values[item.id]) {
+          console.log(values[item.id]);
+          if (values[item.id].length>0) {
             if (values[item.id].length > 0) {
               const value = values[item.id][0];
               valuesData[item.columnLabel] = value[item.labelKey];
