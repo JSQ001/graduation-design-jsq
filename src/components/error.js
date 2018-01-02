3a8c2c2a743628184d0910d9770f34f1d5618d24
ZaraNengap <ZaraNengap@163.com>
2018-01-02 15:21:12 +0800

    temp

--- src/components/error.js
+++ src/components/error.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import 'styles/components/error.scss'
 import errorImg from 'images/error.png'
+import { Button } from 'antd'
 
 class Error extends React.Component {
+
+
+
   render() {
     return (
-      <div className="error">
-        <div className="err-container">
-          <img src={errorImg}/>
+      <div className="error background-transparent">
+        <img src={errorImg}/>
+        <div className="error-message">
+          <div>{this.props.title}</div>
           <div>{this.props.text}</div>
+          <Button onClick={}>{this.props.buttonText}</Button>
         </div>
       </div>
     );
@@ -16,7 +22,19 @@ class Error extends React.Component {
 }
 
 Error.PropTypes = {
-  text: React.PropTypes.string
+  title: React.PropTypes.string,
+  text: React.PropTypes.string,
+  skip: React.PropTypes.string,
+  buttonText: React.PropTypes.string,
+  hasButton: React.PropTypes.bool
+};
+
+Error.defaultProps = {
+  title: '出错啦',
+  text: '请联系重试操作或联系管理员:(',
+  skip: React.PropTypes.string,
+  buttonText: React.PropTypes.string,
+  hasButton: false
 };
 
 export default Error;
